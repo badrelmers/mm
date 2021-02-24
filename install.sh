54133a0390531e57a29c652aee22fac423b5f568
Badr Elmers <40046489+badrelmers@users.noreply.github.com>
2021-02-24 18:31:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${__dir}" || exit 1
 
 # install mm
-sudo cp ./mm/mm /usr/local/bin/mm
+sudo cp mm /usr/local/bin/mm
 sudo chmod +x /usr/local/bin/mm
 
 # install mmuild
-sudo cp ./mm/mmbuild /usr/local/bin/mmbuild
+sudo cp mmbuild /usr/local/bin/mmbuild
 sudo chmod +x /usr/local/bin/mmbuild
