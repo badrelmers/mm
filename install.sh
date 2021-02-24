69f35af9cd2d30a518596c965e59186474219d64
Badr Elmers <40046489+badrelmers@users.noreply.github.com>
2021-02-24 16:27:49 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install mm
+sudo cp ./mm/mm /usr/local/bin/mm
+sudo chmod +x /usr/local/bin/mm
+
+# install mmuild
+sudo cp ./mm/mmbuild /usr/local/bin/mmbuild
+sudo chmod +x /usr/local/bin/mmbuild
